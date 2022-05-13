afb144c4bf4b72dd3d7fc309a7ec9b7252546b35
Kulbhushan Mayer <kulbhushan.mayer@gmail.com>
2022-05-13 10:47:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 resource "aws_iam_role_policy_attachment" "aws_iam_role_policy_attachment" {
   for_each = toset(local.policies)
-  role = module.create_roles_for_eks_cluster["thinknyx_eks_cluster_role-eks"].role_name
+  role = var.role
   policy_arn = "arn:aws:iam::aws:policy/${each.value}"
 }
-  
